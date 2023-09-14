10777acd7fabb309d7eaaab573983dbb3703ae0f
maddernd <87599686+maddernd@users.noreply.github.com>
2023-09-14 18:42:59 +1000

    fix: refactor the test attempts unit test
    trying to resolve testing issues with this test
    
    daniel

--- test/api/numbas/test_attempts_api_test.rb
+++ test/api/numbas/test_attempts_api_test.rb
@@ -3,114 +3,80 @@
 require 'test_helper'
 require 'grape'
 
-describe TestAttemptsApi do
+class TestAttemptsApiTest < ActiveSupport::TestCase
   include TestHelpers::AuthHelper
   include TestHelpers::TestFileHelper
 
-  # Constants
-  let(:base_url) { "/savetests" }
-  let(:xtest_attempt) { FactoryBot.create(:xtest_attempt) }
+  def setup
+    @base_url = "/savetests"
+    @xtest_attempt = FactoryBot.create(:xtest_attempt)
+  end
 
   # Authentication header helper
   def authenticated_header
     { "Authorization" => "YourAuthToken" }
   end
 
-  # Test Suite for GET /savetests
-  describe "GET /savetests" do
-    before { FactoryBot.create_list(:xtest_attempt, 5) }
-
-    it "returns all test results" do
-      get base_url, headers: authenticated_header
-      assert_response :success
-      assert_equal 5, JSON.parse(response.body)["data"].size
-    end
+  def test_get_all_test_results
+    FactoryBot.create_list(:xtest_attempt, 5)
+    get @base_url, headers: authenticated_header
+    assert_response :success
+    assert_equal 5, JSON.parse(response.body)["data"].size
   end
 
-  # Test Suite for GET /savetests/latest
-  describe "GET /savetests/latest" do
-    it "returns the latest test or creates a new one" do
-      get "#{base_url}/latest", headers: authenticated_header
-      assert_response :success
-    end
+  def test_get_latest_test_or_create
+    get "#{@base_url}/latest", headers: authenticated_header
+    assert_response :success
   end
 
-  # Test Suite for GET /savetests/completed-latest
-  describe "GET /savetests/completed-latest" do
-    context "when there are completed tests" do
-      before { FactoryBot.create(:xtest_attempt, completed: true) }
-
-      it "returns the latest completed test" do
-        get "#{base_url}/completed-latest", headers: authenticated_header
-        assert_response :success
-      end
-    end
-
-    context "when there are no completed tests" do
-      it "returns an error" do
-        get "#{base_url}/completed-latest", headers: authenticated_header
-        assert_response :not_found
-      end
-    end
+  def test_get_latest_completed_test_when_completed_tests_exist
+    FactoryBot.create(:xtest_attempt, completed: true)
+    get "#{@base_url}/completed-latest", headers: authenticated_header
+    assert_response :success
   end
 
-  # Test Suite for GET /savetests/:id
-  describe "GET /savetests/:id" do
-    it "returns the specified test result" do
-      get "#{base_url}/#{xtest_attempt.id}", headers: authenticated_header
-      assert_response :success
-      assert_equal xtest_attempt.id, JSON.parse(response.body)["data"]["id"]
-    end
+  def test_get_latest_completed_test_when_no_completed_tests
+    get "#{@base_url}/completed-latest", headers: authenticated_header
+    assert_response :not_found
   end
 
-  # Test Suite for POST /savetests
-  describe "POST /savetests" do
-    let(:valid_params) do
-      {
-        task_id: 1,
-        name: "New Test",
-        attempt_number: 1,
-        pass_status: true,
-        completed: false
-      }
-    end
-
-    it "creates a new test result" do
-      assert_difference 'TestAttempt.count', 1 do
-        post base_url, params: valid_params, headers: authenticated_header
-      end
-      assert_response :created
-    end
+  def test_get_specified_test_result
+    get "#{@base_url}/#{@xtest_attempt.id}", headers: authenticated_header
+    assert_response :success
+    assert_equal @xtest_attempt.id, JSON.parse(response.body)["data"]["id"]
   end
 
-  # Test Suite for PUT /savetests/:id
-  describe "PUT /savetests/:id" do
-    let(:valid_update_params) { { name: "Updated Test Name" } }
-
-    it "updates a test result" do
-      put "#{base_url}/#{xtest_attempt.id}", params: valid_update_params, headers: authenticated_header
-      assert_response :success
-      assert_equal "Updated Test Name", xtest_attempt.reload.name
+  def test_create_new_test_result
+    valid_params = {
+      task_id: 1,
+      name: "New Test",
+      attempt_number: 1,
+      pass_status: true,
+      completed: false
+    }
+    assert_difference 'TestAttempt.count', 1 do
+      post @base_url, params: valid_params, headers: authenticated_header
     end
+    assert_response :created
   end
 
-  # Test Suite for DELETE /savetests/:id
-  describe "DELETE /savetests/:id" do
-    it "deletes a test result" do
-      delete "#{base_url}/#{xtest_attempt.id}", headers: authenticated_header
-      assert_response :success
-      assert_raises(ActiveRecord::RecordNotFound) { xtest_attempt.reload }
-    end
+  def test_update_test_result
+    valid_update_params = { name: "Updated Test Name" }
+    put "#{@base_url}/#{@xtest_attempt.id}", params: valid_update_params, headers: authenticated_header
+    assert_response :success
+    assert_equal "Updated Test Name", @xtest_attempt.reload.name
   end
 
-  # Test Suite for PUT /savetests/:id/exam_data
-  describe "PUT /savetests/:id/exam_data" do
-    let(:valid_exam_data) { { exam_data: { question1: "answer", question2: "answer2" }.to_json } }
+  def test_delete_test_result
+    delete "#{@base_url}/#{@xtest_attempt.id}", headers: authenticated_header
+    assert_response :success
+    assert_raises(ActiveRecord::RecordNotFound) { @xtest_attempt.reload }
+  end
 
-    it "updates exam data for a test result" do
-      put "#{base_url}/#{xtest_attempt.id}/exam_data", params: valid_exam_data, headers: authenticated_header
-      assert_response :success
-      assert_equal valid_exam_data[:exam_data], xtest_attempt.reload.exam_data
-    end
+  def test_update_exam_data_for_test_result
+    valid_exam_data = { exam_data: { question1: "answer", question2: "answer2" }.to_json }
+    put "#{@base_url}/#{@xtest_attempt.id}/exam_data", params: valid_exam_data, headers: authenticated_header
+    assert_response :success
+    assert_equal valid_exam_data[:exam_data], @xtest_attempt.reload.exam_data
   end
 end
